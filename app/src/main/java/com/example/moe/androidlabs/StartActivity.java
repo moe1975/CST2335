2009d497c4083b46e9941a7b79e711329805f8c5
moe1975 <mohamed_abed@outlook.com>
2017-02-06 14:17:21 -0500

    Finished Lab4

--- app/src/main/java/com/example/moe/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/moe/androidlabs/StartActivity.java
@@ -1,7 +1,14 @@
 package com.example.moe.androidlabs;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +16,66 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, ListItemActivity.class);
+                startActivityForResult(intent, 5);
+            }
+        });
+
+        Button buttonChat = (Button) findViewById(R.id.StartChat);
+        buttonChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(ACTIVITY_NAME, "User clicked Start Chat");
+                Intent intent = new Intent(StartActivity.this, ChatWindow.class);
+                startActivity(intent);
+            }
+        });
+
+        Log.i(ACTIVITY_NAME,"In onCreate()");
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent data) {
+        if(requestCode == 5) {
+            Log.i(ACTIVITY_NAME, "Returned to StartActivity.onActivityResult");
+        }
+
+        if(responseCode == Activity.RESULT_OK) {
+            String messagePassed = data.getStringExtra("Response");
+            Toast.makeText(getApplicationContext(), "ListItemsActivity passed: "+messagePassed, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME,"In onResume()");
+
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME,"In onStart()");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME,"In onPause()");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME,"In onStop()");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME,"In onDestroy()");
+    }
+
+    protected static final String ACTIVITY_NAME = "StartActivity";
 }
